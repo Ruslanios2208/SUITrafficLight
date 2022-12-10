866bf45ce792afd0d6b50767df9ff1a7cc968e99
Ruslanios22 <ilgamovich@gmail.com>
2022-12-10 12:06:09 +0300

    Initial Commit

---
+++ SUITrafficLight/SUITrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUITrafficLightApp.swift
+//  SUITrafficLight
+//
+//  Created by Руслан Шигапов on 10.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SUITrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
